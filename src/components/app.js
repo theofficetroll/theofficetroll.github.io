c5f9a3c030dd9ee850e4364036cfa3c3e61319f4
Mark <mark.scrudder@gmail.com>
2021-02-27 15:12:29 -0800

    Portfolio is deployable. Added basics of top bar.

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Topbar from './topbar';
+
+const App = (props) => {
+  return(
+    <div>
+      <Topbar />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/topbar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './topbar.css';
+
+// TODO
+// Make dynamic
+// Put in a logo
+// Make carets into drop-downs
+// Make search clickable and so you can input data
+// Make Sign Up go to an email form
+
+const Topbar = (props) => {
+  return (
+    <div class="navbar">
+      <div class="navbar-button">
+        HumbleLogo
+      </div>
+      <div class="navbar-button">
+        100 Projects
+        <i class="caret-down">
+          &#9660;
+        </i>
+      </div>
+      <div class="navbar-button">
+        Choice
+      </div>
+      <div class="navbar-button">
+        Store
+      </div>
+      <div class="navbar-button">
+        About
+        <i class="caret-down">
+          &#9660;
+        </i>
+      </div>
+      <div class="navbar-button searchbar">
+        <div id="site-search" class="search">
+          {/* <input aria-label="Search" class="search-input" placeholder="Search" /> */}
+          <button class="search-button search-input mag-glass" aria-label="Clear search">
+            &#9906;
+          </button>
+        </div>
+      </div>
+      <div class="navbar-button login-button">
+        Log In
+      </div>
+      <div class="navbar-button signup-button">
+        Sign Up
+      </div>
+    </div>
+  )
+}
+
+export default Topbar;
